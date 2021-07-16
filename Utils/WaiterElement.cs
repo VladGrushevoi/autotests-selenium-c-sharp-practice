5270b449da0cb6d3a72c16c87a1f58a3daf774fd
Vladislav Hrushovyi <vlad.grush.99@gmail.com>
2021-07-16 22:52:46 +0300

    Add new wait method,  which a checking page load

--- Utils/WaiterElement.cs
+++ Utils/WaiterElement.cs
@@ -12,5 +12,17 @@ namespace Utils
             new WebDriverWait(driver, TimeSpan.FromSeconds(seconds)).Until(ExpectedConditions.ElementIsVisible(locator));
             new WebDriverWait(driver, TimeSpan.FromSeconds(seconds)).Until(ExpectedConditions.ElementToBeClickable(locator));
         }
+
+        public static void ShouldLocate(IWebDriver webDriver, string location)
+        {
+            try
+            {
+                new WebDriverWait(webDriver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.UrlContains(location));
+            }
+            catch(WebDriverTimeoutException e)
+            {
+                throw new NotFoundException($"Not found {location}, timeout", e);
+            }
+        }
     }
 }
